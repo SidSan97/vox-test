5e89d71624f224f5fa8d5220ed5098725cfc14d0
SidSan97 <sidnei1.8santiago@hotmail.com>
2025-06-12 00:45:48 -0300

    feat: Criando models para quadro e categorias

---
+++ app/Models/Category.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Category extends Model
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'title',
+        'board_id',
+        'position',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'board_id' => 'integer',
+            'position' => 'integer',
+        ];
+    }
+}
