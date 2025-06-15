24efe0696fcc0694a3af452a2abd71127aa33831
SidSan97 <sidnei1.8santiago@hotmail.com>
2025-06-15 14:50:12 -0300

    style: Adicionando indicador de dropdown no navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -21,7 +21,7 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                         data-bs-toggle="dropdown" aria-expanded="false">
-                        {{ Auth::user()->name }}
+                        {{ Auth::user()->name }} <i class="bi bi-caret-down-fill"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
                         <li><a class="dropdown-item" href="{{ route('profile.edit') }}">Perfil</a></li>
